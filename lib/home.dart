3e4f8b1dbc4cf639436029c39fbdac4d0718ef97
zixxin <74345861+zixxin@users.noreply.github.com>
2022-12-19 16:27:01 +0900

    complete recommendSection

--- lib/home.dart
+++ lib/home.dart
@@ -19,6 +19,7 @@ class _MyHomePageState extends State<MyHomePage> {
             children: <Widget>[
               topSection(context),
               categorySection(),
+              recommendSection(),
             ],
           ),
         ],
@@ -31,7 +32,7 @@ Widget topSection(BuildContext context) {
   double width = MediaQuery.of(context).size.width;
 
   return Container(
-    height: 270,
+    height: 250,
     width: width,
     padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 25),
     color: const Color(0xFF2A6143),
@@ -105,7 +106,7 @@ Widget categorySection() {
         ),
         SizedBox(
           width: 1000,
-          height: 180,
+          height: 150,
           child: ListView(
             padding: const EdgeInsets.symmetric(horizontal: 20),
             scrollDirection: Axis.horizontal,
@@ -137,8 +138,111 @@ Widget categories(String img, String title, String bool) {
               color: const Color(0xFFF5F5F2),
               child: Image.asset(img)),
           Text(title,
-              style: const TextStyle(fontSize: 15, color: Colors.black)),
+              style: const TextStyle(fontSize: 14, color: Colors.black)),
         ],
+      ),
+      if (bool == 'F') const Padding(padding: EdgeInsets.only(right: 15)),
+    ],
+  );
+}
+
+Widget recommendSection() {
+  return Container(
+    margin: const EdgeInsets.symmetric(vertical: 10),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+          child: const Text('Recommended For You',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17)),
+        ),
+        SizedBox(
+          width: 1000,
+          height: 305,
+          child: ListView(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            scrollDirection: Axis.horizontal,
+            children: [
+              products(
+                  'images/recomm1.png',
+                  'adidas Yeezy Slide Bone (2022 Restock)',
+                  'Lowest Ask',
+                  106,
+                  116,
+                  'F'),
+              products(
+                  'images/recomm2.png',
+                  'Nike Dunk Low Retro White Black Panda (2022 Restock)',
+                  'Lowest Ask',
+                  158,
+                  161,
+                  'F'),
+              products(
+                  'images/recomm3.png',
+                  'Jordan 1 Retro Low OGZion Williamson Vood',
+                  'Lowest Ask',
+                  205,
+                  229,
+                  'T'),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget products(
+    String img, String title, String desc, int price, int sale, String bool) {
+  return Row(
+    children: [
+      Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 1,
+            color: const Color(0xFFEBEBEB),
+          ),
+        ),
+        margin: const EdgeInsets.only(top: 20, bottom: 15),
+        padding: const EdgeInsets.all(15),
+        height: 400,
+        width: 150,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(top: 10),
+              margin: const EdgeInsets.only(bottom: 30),
+              child: Image.asset(img),
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 7),
+              child: Text(title,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  style: const TextStyle(fontSize: 14, color: Colors.black)),
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 7),
+              child: Text(desc,
+                  style: const TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF626262))),
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 7),
+              child: Text('\$$price',
+                  style: const TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black)),
+            ),
+            Text('Last Sale: \$$sale',
+                style: const TextStyle(fontSize: 13, color: Color(0xFF626262))),
+          ],
+        ),
       ),
       if (bool == 'F') const Padding(padding: EdgeInsets.only(right: 15)),
     ],
